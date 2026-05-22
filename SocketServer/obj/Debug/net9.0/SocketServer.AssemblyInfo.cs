7ea7d1c2aa4a58bdab8bae8f8b09a059b3c786d6
DESKTOP-842PFAN\NEWBIT-DC2-KHJ <saz575593@gmail.com>
2026-05-22 12:39:05 +0900

    MCP 연동 전 커밋

--- SocketServer/obj/Debug/net9.0/SocketServer.AssemblyInfo.cs
+++ SocketServer/obj/Debug/net9.0/SocketServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocketServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13cc1ed844cbc6f40934b44791e9f26f7d102045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1186397a3a7c9aa15aaf1c78b860b198e7bd3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocketServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocketServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
